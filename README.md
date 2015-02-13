3478912917968c335691081de051b8cd6b75703d
mtbtiago <tiago.ameller@gmail.com>
2015-02-13 09:59:34 +0100

    final version, with mention of solution

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ##Ironhack
 
 ###First angular.js exercise
+
+Solution to the exercise is [here](https://github.com/mtbtiago/tinder-wars)
--- javascript/controllers.js
+++ javascript/controllers.js
@@ -6,9 +6,34 @@ var peopleApp = angular.module('angularApp', []);
 
 peopleApp.controller('PeopleListCtrl', function($scope, $http) { // peopleApp is controller
   $http.get('http://tinder-api.herokuapp.com/people').success(function(data) {
-    $scope.people = data;
+    var people = data;
+
+    people.forEach(function(person) {
+      var latitude = person.latitude;
+      var longitude = person.longitude;
+      var url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+latitude+','+longitude+'&sensor=true_or_false';
+
+      ajax(url, {}, function(response) {
+        var city = response.results[0].address_components[3].long_name;
+        person.city = city;
+        $scope.$apply();
+      });
+    });
+
+    $scope.people = people;
   });
 });
+
+function ajax(url, data, callback) {
+  var xhr = new XMLHttpRequest();
+
+  xhr.open("POST", url);
+  xhr.send(data);
+  xhr.onload = function(event) {
+    var response = JSON.parse(event.target.response);
+    callback(response);
+  };
+};
 
 /* version with hardcoded model
 peopleApp.controller('PeopleListCtrl', function ($scope) { // peopleApp is controller
